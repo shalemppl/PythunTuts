41fff7256974f21aafaea510a9cf7fd661995578
shalemppl <shalem.shemtov@gmail.com>
2019-09-08 14:17:32 -0400

    Added the Get method to the Dict.py file

--- Dict.py
+++ Dict.py
@@ -1,1 +1,16 @@
-# Using a dictionary to count
+# Using a dictionary to count (histogram)
+mydict = {} #creates an empty disctionary; same as mydict = dict()
+names = ['Ella', 'Hava', 'David', 'Hava', 'Ella', 'Ella']
+for name in names:
+    if name not in mydict:
+        mydict[name] = 1   #if the name is not in the dictionary, add it and set it to 1
+    else :
+        mydict[name] = mydict[name] + 1  #if the name i already in the dict, add 1 to it
+print(mydict)
+
+#Do the same thing using the Get method:
+mynewdict = {}
+newnames = ['Ella', 'Hava', 'David', 'Hava', 'Ella', 'Ella']
+for newname in newnames:
+    mynewdict[newname] = mynewdict.get(newname, 0) + 1 #if newname is not in dict add it with default value of 0, otherwise add 1 to it
+print(mynewdict)
